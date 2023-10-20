4117d5452eae749a2dd7659f76859d88f65d0c2e
miriSeo <42762090+miriSeo@users.noreply.github.com>
2023-10-20 23:14:53 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 196 ms, Memory: 17560 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char[] cArr = sc.nextLine().toLowerCase().toCharArray();
-        System.out.println(cArr.length);
+        String s = sc.next();
+        System.out.println(s.length());
     }
 }
