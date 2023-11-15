11e1f579b9508e5571586f1ed3db432f04bfdb58
miriSeo <42762090+miriSeo@users.noreply.github.com>
2023-11-15 19:19:02 +0900

    [Silver IV] Title: 제로, Time: 832 ms, Memory: 109832 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+
+        while (K-- > 0) {
+            int num = sc.nextInt();
+            if (num > 0) {
+                stack.push(num);
+            } else {
+                stack.pop();
+            }
+        }
+
+        System.out.println(stack.stream().mapToInt(Integer::intValue).sum());
+    }
+}
