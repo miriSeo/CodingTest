368fdcbbb9601958b5696479baec5ed251d6749a
miriSeo <42762090+miriSeo@users.noreply.github.com>
2024-03-09 20:53:23 +0900

    [Silver II] Title: 쇠막대기, Time: 364 ms, Memory: 22824 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -3,25 +3,24 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
+        Stack<Character> stack = new Stack<>();
         Scanner sc = new Scanner(System.in);
         char[] cArr = sc.nextLine().toCharArray();
-        Stack<Character> stack = new Stack<>();
         int result = 0;
-
-        for (int i = 0; i < cArr.length; i++) {
-            if (cArr[i] == '(') {
-                stack.push(cArr[i]);
-            } else {
-                stack.pop();
-
-                if (cArr[i - 1] == '(') {
-                    result += stack.size();
-                } else {
-                    result += 1;
-                }
+        char bef = '\0';
+        for (char c : cArr) {
+            switch (c) {
+                case '(':
+                    stack.push(c);  // 쇠막대기 왼쪽 끝
+                    break;
+                case ')':
+                    stack.pop();
+                    if (bef == '(') result += stack.size(); // 레이저 발사
+                    else result += 1;   // 쇠막대기 오른쪽 끝
+                    break;
             }
+            bef = c;
         }
-
         System.out.println(result);
     }
 }
