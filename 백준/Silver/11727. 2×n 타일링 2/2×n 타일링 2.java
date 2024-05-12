bbe55260cfed4f3c1c425a92551316f9bae40a1d
miriSeo <42762090+miriSeo@users.noreply.github.com>
2024-05-12 13:23:12 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 212 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) dp[i] = 1;
+            else if (i == 2) dp[i] = 3;
+            else dp[i] = (dp[i - 1] + (dp[i - 2] * 2)) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
+
+/*
+2*n 사이즈의 직사각형으로 채우려면
+2*(n-1) 직사각형을 채우는 개수 + (2*(n-2) 직사각형을 채우는 개수 * 2)
+ */
