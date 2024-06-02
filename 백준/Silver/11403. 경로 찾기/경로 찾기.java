cb072b9068e0c029dd4cdfbb4b82151d44ea4beb
miriSeo <42762090+miriSeo@users.noreply.github.com>
2024-06-02 23:33:43 +0900

    [Silver I] Title: 경로 찾기, Time: 192 ms, Memory: 15456 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -8,36 +8,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[][] initial = new int[N][N];
-        int[][] result = new int[N][N];
+        int INF = 100;
+        int[][] d = new int[N + 1][N + 1];
 
         // 그래프 초기화
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
+            for (int j = 1; j <= N; j++) {
                 int num = Integer.parseInt(st.nextToken());
-                initial[i][j] = num;
-                result[i][j] = num;
+                d[i][j] = num == 0 ? INF : num;
             }
         }
 
         // k = 거쳐가는 노드
-        for (int k = 0; k < N; k++) {
+        for (int k = 1; k <= N; k++) {
             // i = 출발 노드
-            for (int i = 0; i < N; i++) {
+            for (int i = 1; i <= N; i++) {
                 // j = 도착 노드
-                for (int j = 0; j < N; j++) {
-                    // i에서 k를 거쳐 j로 가는 경로가 있으면 (둘 다 1이라면) 숫자를 1로 출력
-                    if (result[i][k] > 0 && result[k][j] > 0) result[i][j] = 1;
+                for (int j = 1; j <= N; j++) {
+                    // i에서 k를 거쳐 j로 가는 경로가 더 짧으면 숫자 변경
+                    if (d[i][j] > d[i][k] + d[k][j])
+                        d[i][j] = d[i][k] + d[k][j];
                 }
             }
         }
 
         // 출력
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < N; j++) {
-                sb.append(result[i][j]).append(" ");
+            for (int j = 1; j <= N; j++) {
+                sb.append(d[i][j] == INF ? 0 : 1).append(" ");
             }
             System.out.println(sb);
         }
